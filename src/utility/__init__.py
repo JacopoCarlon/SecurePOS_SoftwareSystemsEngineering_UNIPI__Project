9da91b8b8d1b13de3014a795bc9acae7c3181a17
nickrick3 <nik3.riccardi@gmail.com>
2024-12-06 10:44:31 +0100

    utility moved into source

--- src/utility/__init__.py
+++ src/utility/__init__.py
@@ -4,5 +4,5 @@ This module offers some utility variables nd functions
 
 import os
 
-project_root = os.path.realpath(__file__ + "/../..")
+project_root = os.path.realpath(__file__ + "/../../..")
 data_folder = os.path.join(project_root, "data")
