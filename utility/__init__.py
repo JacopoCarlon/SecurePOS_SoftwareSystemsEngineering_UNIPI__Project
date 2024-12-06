dcc026c9bfa5f355c018191ebdbbe2dca68960b4
nickrick3 <nik3.riccardi@gmail.com>
2024-12-06 10:06:42 +0100

    utility for relative paths

---
+++ utility/__init__.py
@@ -0,0 +1,8 @@
+"""
+This module offers some utility variables nd functions
+"""
+
+import os
+
+project_root = os.path.realpath(__file__ + "/../..")
+data_folder = os.path.join(project_root, "data")
