041ea86fed79f16ffcefd8e7b4785e0d5b9fc385
JacopoCarlon <jacopo.equi@gmail.com>
2024-12-09 23:42:24 +0100

    Better LabelStore.

--- src/evaluation_system/label_store.py
+++ src/evaluation_system/label_store.py
@@ -8,9 +8,9 @@ class LabelStore:
     def __init__(self):
         self.db = DatabaseController('evaluationDB.db')
 
-    def ls_store_label(self, label, table):
-        if not self.db.insert(label, table):
-            logging.error(f'Impossible to store the label, \ntrg_table : {table}, \nlabel : {label}')
+    def ls_store_label_df(self, label, table):
+        if not self.db.insert_dataframe(label, table):
+            logging.error(f'Impossible to <insert_dataframe>, \ntarget_table : {table}, \nlabel_df : {label}')
             raise ValueError("Evaluation System label storage failed")
 
     def ls_create_table(self, query, params):
@@ -20,7 +20,7 @@ class LabelStore:
         :return: False if any error has occurred, True otherwise
         """
         if not self.db.create_table(query, params):
-            logging.error(f'Impossible to create the table with : \nquery : {query} ; \nparams : {params}')
+            logging.error(f'Impossible to <create> the table with : \nquery : {query} ; \nparams : {params}')
             raise ValueError("Evaluation System create_table failed")
 
     def ls_delete_labels(self, query, params):
@@ -29,8 +29,8 @@ class LabelStore:
         :param params: params to insert in placeholders in query
         :return: nothing, can raise errors
         """
-        if not self.db.update(query, params):
-            logging.error(f'Impossible to delete labels with : \nquery : {query} ; \nparams : {params}')
+        if not self.db.delete(query, params):
+            logging.error(f'Impossible to <delete> labels with : \nquery : {query} ; \nparams : {params}')
             raise ValueError("Evaluation System create_table failed")
 
     def ls_select_labels(self, query, params):
