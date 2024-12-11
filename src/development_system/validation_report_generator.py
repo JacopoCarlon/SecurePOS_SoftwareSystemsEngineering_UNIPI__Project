8d72fad764eeb2c55e11047439fbac87739bfbe8
nickrick3 <nik3.riccardi@gmail.com>
2024-12-11 18:17:15 +0100

    Test classifier

--- src/development_system/validation_report_generator.py
+++ src/development_system/validation_report_generator.py
@@ -4,6 +4,7 @@ This module contains a class for generating validation reports
 
 import json
 import pandas as pd
+from development_system.classifier_data import ClassifierData
 
 
 class ValidationReportGenerator:
@@ -19,27 +20,23 @@ class ValidationReportGenerator:
         self.overfitting_tolerance = overfitting_tolerance
         self.report_df = pd.DataFrame()
 
-    def add_row(self, classifier_id, layers, neurons, training_error, validation_error) -> None:
+    def add_row(self, classifier_data: ClassifierData) -> None:
         """
         Adds a new row to the report, sorts all rows and keeps only the first 5
-        :param classifier_id: index number of the classifier
-        :param layers: number of layers in the classifier
-        :param neurons: number of neurons in each layer
-        :param training_error: error (1 - accuracy) on training set
-        :param validation_error: error (1 - accuracy) on validation set
+        :param classifier_data: contains data of the classifier
         :return: None
         """
-        error_difference = training_error - validation_error
+        error_difference = classifier_data.training_error - classifier_data.validation_error
         valid = -self.overfitting_tolerance < error_difference < self.overfitting_tolerance
 
         row = pd.DataFrame.from_dict({
-            "classifier_id": [classifier_id],
-            "num_layers": [layers],
-            "num_neurons": [neurons],
-            "training_error": [training_error],
-            "validation_error": [validation_error],
+            "classifier_id":    [classifier_data.classifier_id],
+            "layers":           [classifier_data.layers],
+            "neurons":          [classifier_data.neurons],
+            "training_error":   [classifier_data.training_error],
+            "validation_error": [classifier_data.validation_error],
             "error_difference": [error_difference],
-            "valid": [valid]
+            "valid":            [valid]
         })
 
         self.report_df = pd.concat([self.report_df, row]).sort_values(by='validation_error').head(5)
