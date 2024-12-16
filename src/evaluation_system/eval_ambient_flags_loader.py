679c99fe001100c617811bc4b211fb2c6569b1d5
JacopoCarlon <jacopo.equi@gmail.com>
2024-12-16 18:53:58 +0100

    ambient flags for testing and timing

--- src/evaluation_system/eval_ambient_flags_loader.py
+++ src/evaluation_system/eval_ambient_flags_loader.py
@@ -5,8 +5,8 @@ import json
 from utility.json_validation import validate_json_file_file
 from utility import data_folder
 
-TESTING_CONFIG_PATH_RELATIVE = "evaluation_system/configs/testing_config.json"
-TESTING_CONFIG_SCHEMA_PATH_RELATIVE = "evaluation_system/configs/testing_config_schema.json"
+TESTING_CONFIG_PATH_RELATIVE = "evaluation_system/configs/eval_ambient_flags.json"
+TESTING_CONFIG_SCHEMA_PATH_RELATIVE = "evaluation_system/configs/eval_ambient_flags_schema.json"
 TESTING_VALIDITY = \
     validate_json_file_file(TESTING_CONFIG_PATH_RELATIVE, TESTING_CONFIG_SCHEMA_PATH_RELATIVE)
 
@@ -15,6 +15,12 @@ testing_conf_location = f'{data_folder}/{TESTING_CONFIG_PATH_RELATIVE}'
 with open(testing_conf_location, "r", encoding="UTF-8") as jsonTestingFile:
     testing_config_content = json.load(jsonTestingFile)
 
+DB_NAME = testing_config_content["db_name"]
 TESTING = testing_config_content["testing"] == "True"
+TIMING = testing_config_content["timing"] == "True"
+DELETE_DB_ON_LOAD = testing_config_content["delete_db_on_load"] == "True"
 
-print(f'testing status : {TESTING}')
+print(f'DB_NAME : {DB_NAME}')
+print(f'TESTING status : {TESTING}')
+print(f'TIMING status : {TIMING}')
+print(f'DELETE_DB_ON_LOAD status : {DELETE_DB_ON_LOAD}')
